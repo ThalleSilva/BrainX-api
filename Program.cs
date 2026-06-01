b770bd1f82d55ef7b30285113f510b379b8a3537
Thalles <167459991+ThalleSilva@users.noreply.github.com>
2026-06-01 19:23:47 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -3,18 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionando o SQLite
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=brainx_v2.db"));
 
-// Adiciona suporte aos Controllers
 builder.Services.AddControllers(); 
 
-// Configuração do Swagger UI (Substituindo o antigo AddOpenApi)
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configura o CORS para o React na porta 3000 ou 5173 (Vite geralmente usa 5173, ajustei para suportar ambos)
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
